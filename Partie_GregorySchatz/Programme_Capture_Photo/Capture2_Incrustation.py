0fb7701ed27057fa2307d73740ef4bbc9bb3bc7c
Gregory Schatz <77841067+Greg1007@users.noreply.github.com>
2022-06-02 10:14:05 +0200

    Add files via upload

--- Partie_GregorySchatz/Programme_Capture_Photo/Capture2_Incrustation.py
+++ Partie_GregorySchatz/Programme_Capture_Photo/Capture2_Incrustation.py
@@ -21,20 +21,24 @@ while True:
     try:
         GPIO.wait_for_edge(BUTTON_GPIO, GPIO.FALLING)      
         with picamera.PiCamera() as camera:
+            #reponse = str(reponse)
             # Définition de la résolution des photos
             camera.resolution = (2592, 1944)
             # Récupération de l'heure de l'appareil
             start = dt.datetime.now()
             date_fichier = start.strftime("%F_%T")
             # Requête HTTP pour l'ID de la photo
-            r = requests.get('http://192.168.1.20/Laser/id')
+            r = requests.get('http://192.168.1.20/laser/id')
             reponse = r.text
+
+
             # Incrustation du nom de l'image sur l'image
-            camera.annotate_text = reponse + "_" +date_fichier + "_CAM1"
+            camera.annotate_text = reponse + "_CAM1"
             camera.annotate_text_size = 120
             sleep (0.01)
             # Capture de l'image et nommage avec la date et l'ID
-            camera.capture('/home/pi/Photos/''%s%s%s%s.jpg' % (reponse , "_" , date_fichier , "_CAM1"))
+            camera.capture('/home/pi/Photos/''%s%s.jpg' % (reponse , "_CAM1"))
+            #reponse = int(reponse) + 1
     except:
         time.sleep(0.01)
 
