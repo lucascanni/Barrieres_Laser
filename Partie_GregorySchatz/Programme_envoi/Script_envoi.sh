06ec9eecef3ebcabf465f7a9ef67e7ae1e32efff
Gregory Schatz <77841067+Greg1007@users.noreply.github.com>
2022-06-02 10:14:30 +0200

    Add files via upload

--- Partie_GregorySchatz/Programme_envoi/Script_envoi.sh
+++ Partie_GregorySchatz/Programme_envoi/Script_envoi.sh
@@ -2,13 +2,14 @@
 
 while true
 do
-        DIR=$(ls -a /home/pi/Photos| sed -e "/\.$/d")
+        DIR=$(ls -a /home/pi/Photos|  wc -l )
 
         if [ -n "$DIR" ]; then
-               
-				sshpass -p "raspberry" scp -r /home/pi/Photos pi@192.168.1.20:/var/www/html/img/
-				#cd /home/pi/Photos/
-				#rm -f *
+                
+                #sleep 1
+                sshpass -p "raspberry" scp -r /home/pi/Photos pi@192.168.1.20:/var/www/html/
+                cd /home/pi/Photos/
+                rm -f *
                 
         fi
 
